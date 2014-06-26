c21858ace644a0a421a9256480411ab78e7d16fd
David Reyna Barreto <reyna_b@upeu.edu.pe>
2014-06-26 14:50:47 -0500

    Update Conexion.java

--- PruebaReyna/src/pe/edu/upeu/pruebareyna/config/Conexion.java
+++ PruebaReyna/src/pe/edu/upeu/pruebareyna/config/Conexion.java
@@ -11,5 +11,24 @@ package pe.edu.upeu.pruebareyna.config;
  * @author admin-harold.rojas
  */
 public class Conexion {
+    private static Connection conex = null;
+    private static String url = "jdbc:mysql://localhost/bd_visitas";
+    private static String usuario = "root";
+    private static String clave = "root";
+    
+    public static Connection getConexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            if(conex==null){
+            conex = DriverManager.getConnection(url,usuario,clave);
+            }else{
+                conex = null;
+            }
+        }catch(Excepcion e){
+            JOptionPane.showMessage(null, e);
+        }
+        return conex;
+        
+    }
     
 }
